cc5b41a3e73a1ec22270610c7d704013d46effde
Maris Riba <mariba1986@users.noreply.github.com>
2020-02-05 21:50:16 +0200

    kodune ülesanne lahendatud

--- page_tunnis.php
+++ page_tunnis.php
@@ -4,25 +4,14 @@
 	//<p> Lehe avamise hetkel oli: <strong>31.01.2020 11:32:07</strong> </p>
 	$timeHTML = "\n <p> Lehe avamise hetkel oli: <strong>" .$fullTimeNow ."</strong> </p> \n";
 	$hourNow = date("H");
-	//$partOfDay = "hägune aeg";
+	$partOfDay = "hägune aeg";
 	
 	if($hourNow < 10) {
-        $partOfDay = "hommik";
-		$backColor = '#7CBDC2';
-		$fontColor = '#407999';
-    }
-	elseif($hourNow >= 10 and $hourNow < 18) {
-        $partOfDay = "aeg aktiivselt tegutseda";
-		$backColor = '#50C30A';
-		$fontColor = '#324726';
+		$partOfDay = "hommik";
 	}
-	else {
-        $partOfDay = "hägune aeg";
-		$backColor = '#B2A29F';
-		$fontColor = '#7C463C';
-		
+	if($hourNow >= 10 and $hourNow < 18) {
+		$partOfDay = "aeg aktiivselt tegutseda";
 	}
-
 	$partOfDayHTML = "<p> Käes on " .$partOfDay ." !</p> \n"; 
 	
 	//info semestri kulgemise kohta
@@ -41,15 +30,7 @@
 	$semesterProgressHTML .= '"value = "';
 	$semesterProgressHTML .= $fromSemesterStart->format("%r%a");	
 	$semesterProgressHTML .= '"></meter>.</p>' ."\n"; 
-    
-    //Väljundid kui semester ei ole veel alanud ja kui on juba lõppenud
-    if($today < $semesterStart ){
-        $semesterProgressHTML = "<p> Semester ei ole veel alanud. </p>" ."\n";
-    }
-    if ($today > $semesterEnd){
-        $semesterProgressHTML = "<p> Semester on juba kahjuks lõppenud. </p>" ."\n";
-    }
-
+	
 	//loen etteantud kataloogist pildifailid
 	$picsDir= "../Pics/";
 	$photoTypesAllowed = ["image/jpeg", "image/png"];  //massiv et millised foto tüübid on lubatud
@@ -59,52 +40,29 @@
 	foreach($allFiles as $file){
 		$fileInfo = getimagesize($picsDir .$file);
 		if(in_array($fileInfo["mime"],$photoTypesAllowed) == true) {
-			array_push($photoList, $file);  //lisab iga tingimustele vastava foto massiivi
+			array_push($photoList, $file);
 		}
 	}
 	
 	$photoCount = count($photoList);
-	//$photoNum = mt_rand(0, $photoCount -1 ); 	//mt_rand on kiirem kui lihtsalt rand
-	//$randomImageHTML = '<img src = "' .$picsDir .$photoList[$photoNum] .'" alt="juhuslik pilt Haapsalust">' ."\n";
-
-   
-    $photoNum = [];
-    foreach ([0,1,2] as $element) {
-        do {
-            $drawNum = mt_rand(0, $photoCount-1); 
-		} while (in_array($drawNum, $photoNum)); {
-			array_push($photoNum, $drawNum); //lisab loositud numbrid massiivi
-		}
-    }
-///tsükkel et välja tuleks erinevad pildid
-    $randomImageHTML = '';
-    foreach ($photoNum as $drawNum) {
-        $randomImageHTML .= '<img src="'. $picsDir . $photoList[$drawNum].'" alt="juhuslik pilt Haapsalust" width="250" height="250"/>' ."\n";
-    }	
-
-
+	$photoNum = mt_rand(0, $photoCount -1 ); 		//mt_rand on kiirem kui lihtsalt rand
+	$randomImageHTML = '<img src = "' .$picsDir .$photoList[$photoNum] .'" alt="juhuslik pilt Haapsalust">' ."\n";
+	
 ?>
 <!DOCTYPE html>
 <html lang="et">
 <head>
-<style>
- body {background-color: <?php echo $backColor; ?>;} h1 {color: <?php echo $fontColor; ?>;} p {color:<?php echo $fontColor; ?>;}
-  </style>
-
 	<meta charset="utf-8">
 	<title>Veebirakendused ja nende loomine 2020</title>
-			
 </head>
 <body>
-	
 	<h1><?php echo $myName; ?> </h1>
 	<p>See leht on valminud õppetöö raames!</p>
 	<?php 
 		echo $timeHTML;
 		echo $partOfDayHTML;
 		echo $semesterProgressHTML;
-        echo $randomImageHTML;
-        
+		echo $randomImageHTML;
 	?>
 </body>
 </html>
